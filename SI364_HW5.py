fcfbd4dc23f3ebe5e9749095f4fdb8f954f2ad52
Lauren Murphy <laumurph@umich.edu>
2017-11-19 20:01:09 -0500

    adds final project changes and fixes email format

--- SI364_HW5.py
+++ SI364_HW5.py
@@ -12,6 +12,10 @@ from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate, MigrateCommand
 
+from flask_mail import Mail, Message
+from threading import Thread
+from werkzeug import secure_filename
+
 # Configure base directory of app
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,9 +25,9 @@ app.debug = True
 app.config['SECRET_KEY'] = 'hardtoguessstringfromsi364(thisisnotsupersecure)'
 ## Create a database in postgresql in the code line below, and fill in your app's database URI. It should be of the format: postgresql://localhost/YOUR_DATABASE_NAME
 
-## TODO: Create database and change the SQLAlchemy Database URI.
+## DONE: Create database and change the SQLAlchemy Database URI.
 ## Your Postgres database should be your uniqname, plus HW5, e.g. "jczettaHW5" or "maupandeHW5"
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/hw5_364"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://localhost/laumurphHW5"
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -31,20 +35,53 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # NOTE: Make sure that you DO NOT write your actual email password in text!!!!
 # NOTE: You will need to use a gmail account to follow the examples in the textbook, and you can create one of those for free, if you want. In THIS application, you should use the username and password from the environment variables, as directed in the textbook. So when WE run your app, we will be using OUR email, not yours.
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587 #default
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME') # TODO export to your environs -- may want a new account just for this. It's expecting gmail, not umich
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['MAIL_SUBJECT_PREFIX'] = '[Homework 5 LAUMURPH]'
+app.config['MAIL_SENDER'] = 'Admin <>' # TODO fill in email
+app.config['ADMIN'] = os.environ.get('ADMIN')
+
+
 # Set up Flask debug stuff
 manager = Manager(app)
 db = SQLAlchemy(app) # For database use
 migrate = Migrate(app, db) # For database use/updating
 manager.add_command('db', MigrateCommand) # Add migrate
-# TODO: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
-
+# DONE: Run commands to create your migrations folder and get ready to create a first migration, as shown in the textbook and in class.
+mail = Mail(app) # For email sending
 ## Set up Shell context so it's easy to use the shell to debug
 def make_shell_context():
     return dict(app=app, db=db, Tweet=Tweet, User=User, Hashtag=Hashtag)
 # Add function use to manager
 manager.add_command("shell", Shell(make_context=make_shell_context))
 
-# TODO: Write a send_email function here. (As shown in examples.)
+# # TODO: Write a send_email function here. (As shown in examples.)
+# app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+# app.config['MAIL_PORT'] = 587
+# app.config['MAIL_USE_TLS'] = True
+# app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+# app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+# app.config['MAIL_SUBJECT_PREFIX'] = '[Homework 5 LAUMURPH]'
+# app.config['MAIL_SENDER'] = 'Admin <>'
+# app.config['ADMIN'] = os.environ.get('ADMIN')
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg) #use the mail application to send the email
+
+def send_email(to, subject, template, **kwargs): # kwargs = 'keyword arguments', this syntax means to unpack any keyword arguments into the function in the invocation...
+    msg = Message(app.config['MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['MAIL_SENDER'], recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg]) # using the async email to make sure the email sending doesn't take up all the "app energy" -- the main thread -- at once
+    thr.start()
+    return thr # The thread being returned
+    # However, if your app sends a LOT of email, it'll be better to set up some additional "queuing" software libraries to handle it. But we don't need to do that yet. Not quite enough users!
 
 #########
 ######### Everything above this line is important/useful setup, not problem-solving.
@@ -67,12 +104,13 @@ class Tweet(db.Model):
     def __repr__(self):
         return "{}, (ID: {})".format(self.text,self.id)
 
-# TODO: Add and run a  migration so that each twitter username also saves an associated email.
+# DONE: Add and run a  migration so that each twitter username also saves an associated email.
 # User model
 class User(db.Model):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True) ## -- id (Primary Key)
     twitter_username = db.Column(db.String(64), unique=True)
+    email = db.Column(db.String(64))
     def __repr__(self):
         return "{} (ID: {})".format(self.twitter_username,self.id)
 
@@ -88,11 +126,12 @@ class Hashtag(db.Model):
 
 ##### Set up Forms #####
 
-# TODO: Add a field in the form for a user to enter their email (the email that goes with the twitter username entered).
-# TODO: Edit the template that renders the form so that email is also asked for!
+# DONE: Add a field in the form for a user to enter their email (the email that goes with the twitter username entered).
+# DONE: Edit the template that renders the form so that email is also asked for!
 class TweetForm(FlaskForm):
     text = StringField("What is the text of your tweet? Please separate all hashtags with commas in this case. e.g. 'Yay Python #python, #programming, #awesome' ", validators=[Required()])
     username = StringField("What is your Twitter username?",validators=[Required()])
+    email = StringField("What is your email for this account?", validators=[Required()])
     submit = SubmitField('Submit')
 
 
@@ -105,12 +144,12 @@ class TweetForm(FlaskForm):
 ## -- Hashtags should be identified by their text (e.g. if there's already a hashtag with that text, return it; otherwise, create it)
 
 # TODO: Edit get_or_create_user (AND get_or_create_tweet -- see below) as necessary to store a user's email as well as their twitter username. The get_or_create_user function should accept an email as input and deal with it appropriately to save it as part of a User row. Each user (from now on) has an email! This should be a small change to how the function currently works.
-def get_or_create_user(db_session, username):
+def get_or_create_user(db_session, username, user_email):
     user = db_session.query(User).filter_by(twitter_username=username).first()
     if user:
         return user
     else:
-        user = User(twitter_username=username)
+        user = User(twitter_username=username, email=user_email)
         db_session.add(user)
         db_session.commit()
         return user
@@ -126,12 +165,12 @@ def get_or_create_hashtag(db_session, hashtag_given):
         return hashtag
 
 # TODO: You will need to make changes in this function as well, to address users having emails. See above. What do you need to change to make sure get_or_create_user is *invoked* correctly, including saving an email? Does anything need to change about the input to get_or_create_tweet, and *its* invocations?
-def get_or_create_tweet(db_session, input_text, username):
-    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username).id).first()
+def get_or_create_tweet(db_session, input_text, username, user_email):
+    tweet = db_session.query(Tweet).filter_by(text=input_text, user_id=get_or_create_user(db_session, username, user_email).id).first()
     if tweet:
         return tweet
     else:
-        user = get_or_create_user(db_session, username)
+        user = get_or_create_user(db_session, username, user_email)
         tweet = Tweet(text=input_text, user_id=user.id)
         for text in input_text.split(','):
             if "#" in text.strip():
@@ -164,9 +203,11 @@ def index():
     num_tweets = len(tweets)
     form = TweetForm()
     if form.validate_on_submit():
-        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data).id)).first():
+        if db.session.query(Tweet).filter_by(text=form.text.data, user_id= (get_or_create_user(db.session, form.username.data, form.email.data).id)).first():
             flash("You've already saved that tweet by this user!")
-        get_or_create_tweet(db.session, form.text.data, form.username.data)
+        else:
+            get_or_create_tweet(db.session, form.text.data, form.username.data, form.email.data) ## send mail needs to be fixed
+            send_email(app.config['ADMIN'], 'New Tweet','mail/new_tweet', song=form.text.data)
         return redirect(url_for('see_all_tweets'))
     return render_template('index.html', form=form,num_tweets=num_tweets)
 
--- final_project_app_qs.md
+++ final_project_app_qs.md
@@ -3,39 +3,59 @@
 ## Overall
 
 * **What's a one-two sentence description of what your app will do?**
+* An app that will allow you and others to sign in and build up a pokemon team of your own.
 
 ## The Data
 
 * **What data will your app use? From where will you get it? (e.g. scraping a site? what site? -- careful not to run it too much. An API? Which API?)**
+* The app will use data from the Pokeapi API to get data about pokemon.
 
 * **What data will a user need to enter into a form?**
+* When a user is creating their account, they'll need to add a username, a pokemon region, and optionally a photo to have as a 'user profile image'.
+* an additional form can be used to help build up your pokemon team, where you provide a name or ask for a certain type of pokemon (like grass type) and then results will be sent back to the user which they can add to their team.
 
 * **How many fields will your form have? What's an example of some data user might enter into it?**
+* the first form for creating a provile will need 3 forms, 2 required and 1 optional. The username will need to be a text field, the region can be a dropdown menu to make sure that it's spelt correctly, and the third will be a file upload field (not including the submit field).
+* the second form will need likely 2 fields, where at least one is required, but the first would be a specific name, the second would be a type, so that you can choose to 'search' by one or the other.
 
 * **After a user enters data into the form, what happens? Does that data help a user search for more data? Does that data get saved in a database? Does that determine what already-saved data the user should see?**
+* The first form saves the data into a database. The second form helps the user search, so it won't necessarily get data from a database or save it into one, but it does determine what the user should see next.
 
 * **What models will you have in your application?**
+* I'll need a model for the user, a model for pokemon, a relational database to connect users to pokemon, a model for regions, a model for towns, a relational database between regions and towns, and a relational database between towns and pokemon (potentially a relational database between regions and pokemon, if I want to build up that functionality).
 
 * **What fields will each model have?**
+* the user model will need a username, a userid, a pokemonteamid, and a picturelocation.
+* the pokemon model will need a pokemonid, a type, a typeid, a trainerid, and fields for stats (such as strength or hp, if I want to display information about how strong a team of pokemon is.)
+* the region will need a name, regionid, and townids
+* the towns will need a townname, townid, and pokemonids
 
 * **What uniqueness constraints will there be on each table? (e.g. can't add a song with the same title as an existing song)**
+* Usernames need to be unique and each pokemon needs to be unique on a team (so you can't make up a team of only pikachu for example). 
 
 * **What relationships will exist between the tables? What's a 1:many relationship between? What about a many:many relationship?**
+* A 1:many relationship relationship between regions and towns, regions and trainers, and a many:many relationship between trainers and pokemon as well as towns and pokemon (since towns can spawn pokemon so you can show trainers where they can find pokemon).
 
 * **How many get_or_create functions will you need? In what order will you invoke them? Which one will need to invoke at least one of the others?**
+* I'll need one for each database that's not an association table(so about 4). I'll invoke the user one first, then region, then pokemon, then towns. user will need to invoke the region one, which will also need to invoke the pokemon one. The town will need to invoke the region and the pokemon databases.
 
 ## The Pages
 
 * **How many pages (routes) will your application have?**
+* 1 for the intial page, 1 for the form to make a user, 1 for logging in (could be the initial page), 1 for looking at user information, 1 for looking up information, 1 for listing pokemon to choose from, 1 for a speific pokemon, 1 for region information, 1 for town information. 1 to switch users or log out. Looks like 10 without counting error pages.
 
 * **How many different views will a user be able to see, NOT counting errors?**
+* 10, not including error pages.
 
 * **Basically, what will a user see on each page / at each route? Will it change depending on something else -- e.g. they see a form if they haven't submitted anything, but they see a list of things if they have?**
+* see above answer for each page/route. The option to add a pokemon to one's team is only viewable if the user is logged in.
 
 ## Extras
 
 * **Why might your application send email?**
+* It's possible that I'll want emails so that I'm updated when a new user is added.
 
 * **If you plan to have user accounts, what information will be specific to a user account? What can you only see if you're logged in? What will you see if you're not logged in -- anything?**
-
+* particular teams of pokemon will be specific to a user account. You can only look at user specific information if you're logged in, and you can only add pokemon to your team if you're logged in. Otherwise you can look up pokemon, region, and town information without being logged in.
 * **What are your biggest concerns about the process of building this application?**
+I suppose my biggest concerns will be creating the get_or_create function since so many of the databases rely on each other and getting the user authentication right, since we haven't learned it quite yet.
